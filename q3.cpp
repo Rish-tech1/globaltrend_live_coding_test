c3cdc8e79b2ce30e7eab366018524f3688d05a25
Rishabh Jain <jainrishabh8153@gmail.com>
2024-07-26 16:37:47 +0530

    change the code in q3

--- q3.cpp
+++ q3.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-
 int knapsack(const vector<int>& weights, const vector<int>& values, int capacity) {
     int n = weights.size();
-    vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
-    
-    
-    for (int i = 1; i <= n; ++i) {
-        for (int w = 0; w <= capacity; ++w) {
-            if (weights[i - 1] <= w) {
-                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
+    int dp[n + 1][capacity + 1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= capacity; w++) {
+            if (i == 0 || w == 0) {
+                dp[i][w] = 0;
+            } else if (weights[i - 1] <= w) {
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
             } else {
                 dp[i][w] = dp[i - 1][w];
             }
         }
     }
-    
-    
+
     return dp[n][capacity];
 }
 
@@ -28,10 +26,9 @@ int main() {
     vector<int> weights = {1, 2, 3};
     vector<int> values = {10, 15, 40};
     int capacity = 6;
-    
-    int maxValue = knapsack(weights, values, capacity);
-    
-    cout << "Maximum value that can be obtained: " << maxValue << endl;
-    
+
+    int max_value = knapsack(weights, values, capacity);
+    cout << "Maximum value: " << max_value << endl;
+
     return 0;
 }
